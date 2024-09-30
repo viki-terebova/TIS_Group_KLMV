b70d61227894a58ba20e00f151ea0be0b04f2c71
Klara <senderakova.klara@gmail.com>
2024-09-30 17:15:38 +0200

    chybna oprava sk

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("move stone 1 from %d to %d\n", a, b);
+    printf("presun prvy kamen %d na %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a);
-    printf("move stone %d from %d to %d\n", n, a, b);
+    printf("nic nerob %d z %d na %d\n", n, a, b);
     hanoi(n, 6 - a - b, b);
   }
 }
